4e17b72381346c507ea5a90e9a778949883c4aed
LARISSA NUNES VAZ ALVES DE OLIVEIRA <22102365@dsn127m1246726.cfp127aluno.local>
2023-06-02 08:35:42 -0300

    chat mobile

---
+++ app/src/main/java/com/example/chattutorial/MessagesActivity.kt
@@ -0,0 +1,243 @@
+package com.example.chattutorial
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import io.getstream.chat.android.compose.ui.messages.MessagesScreen
+import io.getstream.chat.android.compose.ui.theme.ChatTheme
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.unit.dp
+import io.getstream.chat.android.compose.ui.theme.StreamShapes
+import androidx.activity.viewModels
+import io.getstream.chat.android.compose.viewmodel.messages.AttachmentsPickerViewModel
+import io.getstream.chat.android.compose.viewmodel.messages.MessageComposerViewModel
+import io.getstream.chat.android.compose.viewmodel.messages.MessageListViewModel
+import io.getstream.chat.android.compose.viewmodel.messages.MessagesViewModelFactory
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import io.getstream.chat.android.common.state.MessageMode.MessageThread
+import io.getstream.chat.android.compose.state.messages.SelectedMessageOptionsState
+import io.getstream.chat.android.compose.state.messages.SelectedMessageReactionsState
+import io.getstream.chat.android.compose.ui.components.messageoptions.defaultMessageOptionsState
+import io.getstream.chat.android.compose.ui.components.selectedmessage.SelectedMessageMenu
+import io.getstream.chat.android.compose.ui.components.selectedmessage.SelectedReactionsMenu
+import io.getstream.chat.android.compose.ui.messages.attachments.AttachmentsPicker
+import io.getstream.chat.android.compose.ui.messages.composer.MessageComposer
+import io.getstream.chat.android.compose.ui.messages.list.MessageList
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Keyboard
+import io.getstream.chat.android.compose.ui.components.composer.MessageInput
+
+
+
+
+
+class MessagesActivity : ComponentActivity() {
+
+    // Build the ViewModel factory
+    private val factory by lazy {
+        MessagesViewModelFactory(
+            context = this,
+            channelId = intent.getStringExtra(KEY_CHANNEL_ID) ?: "",
+        )
+    }
+
+    // Build the required ViewModels, using the 'factory'
+    private val listViewModel: MessageListViewModel by viewModels { factory }
+    private val attachmentsPickerViewModel: AttachmentsPickerViewModel by viewModels { factory }
+    private val composerViewModel: MessageComposerViewModel by viewModels { factory }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // 1 - Load the ID of the selected channel
+        val channelId = intent.getStringExtra(KEY_CHANNEL_ID)
+
+        if (channelId == null) {
+            finish()
+            return
+        }
+
+        // 2 - Add the MessagesScreen to your UI
+        setContent {
+            ChatTheme(
+                shapes = StreamShapes.defaultShapes().copy(
+                    avatar = RoundedCornerShape(8.dp),
+                    attachment = RoundedCornerShape(16.dp),
+                    myMessageBubble = RoundedCornerShape(16.dp),
+                    otherMessageBubble = RoundedCornerShape(16.dp),
+                    inputField = RectangleShape,
+                )
+            ) {
+                MyCustomUi()
+            }
+        }
+    }
+
+    // 3 - Create an intent to start this Activity, with a given channelId
+    companion object {
+        private const val KEY_CHANNEL_ID = "channelId"
+
+        fun getIntent(context: Context, channelId: String): Intent {
+            return Intent(context, MessagesActivity::class.java).apply {
+                putExtra(KEY_CHANNEL_ID, channelId)
+            }
+        }
+    }
+    @Composable
+    fun MyCustomComposer() {
+        MessageComposer( // 1 - Use our MessageComposer as the base component
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight(),
+            viewModel = composerViewModel,
+            integrations = {}, // 2 - Remove integrations from the composer
+            input = { inputState -> // 3 - Add a custom message input
+                MessageInput(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(7f)
+                        .padding(start = 8.dp)
+                        .align(Alignment.CenterVertically),
+                    messageComposerState = inputState,
+                    onValueChange = { composerViewModel.setMessageInput(it) },
+                    onAttachmentRemoved = { composerViewModel.removeSelectedAttachment(it) },
+                    label = { // 4 - Override the label to show a custom icon and a text
+                        Row(
+                            Modifier.wrapContentWidth(),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.Keyboard,
+                                contentDescription = null,
+                                tint = ChatTheme.colors.textLowEmphasis
+                            )
+
+                            Text(
+                                modifier = Modifier.padding(start = 4.dp),
+                                text = "Type something",
+                                color = ChatTheme.colors.textLowEmphasis
+                            )
+                        }
+                    }
+                )
+            }
+        )
+    }
+
+    @Composable
+    fun MyCustomUi() {
+        // 1 - Load the data
+        val isShowingAttachments = attachmentsPickerViewModel.isShowingAttachments
+        val selectedMessageState = listViewModel.currentMessagesState.selectedMessageState
+        val user by listViewModel.user.collectAsState()
+
+        Box(modifier = Modifier.fillMaxSize()) {
+            Scaffold(
+                modifier = Modifier.fillMaxSize(),
+                bottomBar = {
+                    MyCustomComposer() // <--
+                },
+            ) {
+                MessageList( // 4 - Build the MessageList and connect the actions
+                    modifier = Modifier
+                        .background(ChatTheme.colors.appBackground)
+                        .padding(it)
+                        .fillMaxSize(),
+                    viewModel = listViewModel,
+                    onThreadClick = { message ->
+                        composerViewModel.setMessageMode(MessageThread(message))
+                        listViewModel.openMessageThread(message)
+                    }
+                )
+            }
+
+            // 5 - Show attachments when necessary
+            if (isShowingAttachments) {
+                AttachmentsPicker(
+                    attachmentsPickerViewModel = attachmentsPickerViewModel,
+                    modifier = Modifier
+                        .align(Alignment.BottomCenter)
+                        .height(350.dp),
+                    onAttachmentsSelected = { attachments ->
+                        attachmentsPickerViewModel.changeAttachmentState(false)
+                        composerViewModel.addSelectedAttachments(attachments)
+                    },
+                    onDismiss = {
+                        attachmentsPickerViewModel.changeAttachmentState(false)
+                        attachmentsPickerViewModel.dismissAttachments()
+                    }
+                )
+            }
+
+            // 6 - Show the overlay if we've selected a message
+            if (selectedMessageState != null) {
+                val selectedMessage = selectedMessageState.message
+                if (selectedMessageState is SelectedMessageOptionsState) {
+                    SelectedMessageMenu(
+                        modifier = Modifier
+                            .align(Alignment.Center)
+                            .padding(horizontal = 20.dp)
+                            .wrapContentSize(),
+                        shape = ChatTheme.shapes.attachment,
+                        messageOptions = defaultMessageOptionsState(
+                            selectedMessage,
+                            user,
+                            listViewModel.isInThread,
+                            selectedMessageState.ownCapabilities
+                        ),
+                        message = selectedMessage,
+                        onMessageAction = { action ->
+                            composerViewModel.performMessageAction(action)
+                            listViewModel.performMessageAction(action)
+                        },
+                        onShowMoreReactionsSelected = {
+                            listViewModel.selectExtendedReactions(selectedMessage)
+                        },
+                        onDismiss = { listViewModel.removeOverlay() },
+                        ownCapabilities = selectedMessageState.ownCapabilities
+                    )
+                } else if (selectedMessageState is SelectedMessageReactionsState) {
+                    SelectedReactionsMenu(
+                        modifier = Modifier
+                            .align(Alignment.Center)
+                            .padding(horizontal = 20.dp)
+                            .wrapContentSize(),
+                        shape = ChatTheme.shapes.attachment,
+                        message = selectedMessage,
+                        currentUser = user,
+                        onMessageAction = { action ->
+                            composerViewModel.performMessageAction(action)
+                            listViewModel.performMessageAction(action)
+                        },
+                        onShowMoreReactionsSelected = {
+                            listViewModel.selectExtendedReactions(selectedMessage)
+                        },
+                        onDismiss = { listViewModel.removeOverlay() },
+                        ownCapabilities = selectedMessageState.ownCapabilities
+                    )
+                }
+            }
+        }
+    }
+}
+
